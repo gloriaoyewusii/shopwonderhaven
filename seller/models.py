c291223afca28370fef4e4e61c3d84f6a8fafd73
gloryacodes <gloriaoyewusii@gmail.com>
2025-04-22 07:43:02 +0100

    Seller model committed

---
+++ seller/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Seller(models.Model):
+    id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=100, null=False, blank=False)
+    last_name = models.CharField(max_length=100, null=False, blank=False)
+    email = models.CharField(max_length=100, null=False, blank=False)
+    password = models.CharField(max_length=100, null=False, blank=False)
+# Create your models here.
