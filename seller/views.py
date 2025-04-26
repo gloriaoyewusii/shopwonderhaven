9cbe19b75089ccf89c927004a1c6ec2fc14977cf
gloryacodes <gloriaoyewusii@gmail.com>
2025-04-26 21:16:10 +0100

    register seller url path mapped to function and successful

--- seller/views.py
+++ seller/views.py
@@ -1,10 +1,22 @@
+
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+# from rest_framework.utils import json
 
 from seller.sellerservice import SellerService
 
+@api_view(['POST'])
+def register_seller(request):
+    seller_data = request.data
+    print(seller_data)
+    # v_data = json.load(seller_data)
+    # print(v_data)
+    SellerService.register(seller_data)
+    return Response({'message': 'Registration successful'}, status=status.HTTP_201_CREATED)
 
-def register_seller(seller_data):
-    return SellerService.register(seller_data)
+    # return SellerService.register(seller_data)
 
 def submit_item(item_data):
     return SellerService.submit_item(item_data)
